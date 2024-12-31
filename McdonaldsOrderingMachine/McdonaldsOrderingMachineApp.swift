02f7b2352df8468e0e11f6707522359b09b1e57e
Jacob Mobin <jacobamobin@gmail.com>
2024-12-31 17:08:50 -0500

    Initial Commit

---
+++ McdonaldsOrderingMachine/McdonaldsOrderingMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  McdonaldsOrderingMachineApp.swift
+//  McdonaldsOrderingMachine
+//
+//  Created by Jacob Mobin on 2024-12-31.
+//
+
+import SwiftUI
+
+@main
+struct McdonaldsOrderingMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
